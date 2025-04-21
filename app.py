8a84f0528deda21e3f75c9fcb6dda75934e895df
yuhano <jonglee32@naver.com>
2025-04-22 02:32:33 +0900

    [vul/fix] CWE-614: Enable SESSION_COOKIE_SECURE for enhanced security

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ socketio = SocketIO(app)
 csrf = CSRFProtect(app)
 
 app.config.update(
-    # SESSION_COOKIE_SECURE = True,       # HTTPS 전용
+    SESSION_COOKIE_SECURE = True,       # HTTPS 전용
     SESSION_COOKIE_HTTPONLY = True,     # JS 접근 차단
     SESSION_COOKIE_SAMESITE = "Lax",    # 필요하면 Strict
     PERMANENT_SESSION_LIFETIME = 3600,  # 1시간(필요에 맞게)
